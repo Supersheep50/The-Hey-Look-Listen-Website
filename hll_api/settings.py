7ae101b975be405defadd0f36255d6e5cf1b72bf
Supersheep50 <jonathan.morrisseyto@gmail.com>
2023-07-21 14:23:49 +0000

    Fix indentation errors causing app to not load

--- hll_api/settings.py
+++ hll_api/settings.py
@@ -57,12 +57,12 @@ REST_AUTH_SERIALIZERS = {
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
- SECRET_KEY = os.getenv('SECRET_KEY')
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
- ALLOWED_HOSTS = ['localhost', 'hey-look-listen.herokuapp.com']
+ALLOWED_HOSTS = ['localhost', 'hey-look-listen.herokuapp.com']
 
 # Application definition
 
@@ -141,13 +141,13 @@ WSGI_APPLICATION = 'hll_api.wsgi.application'
 if 'DEV' in os.environ:
     DATABASES = {
          'default': {
-             'ENGINE': 'django.db.backends.sqlite3',
-             'NAME': BASE_DIR / 'db.sqlite3',
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
          }
      }
 else:
     DATABASES = {
-         'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
+        'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
      }
 
 
