dd902ba0f8021e1e1a02bc4c52288d265e5e35d5
Supersheep50 <jonathan.morrisseyto@gmail.com>
2023-08-04 09:21:43 +0000

    Refactored code for navbar.js, signinform and app.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,28 +5,14 @@ import { Route, Switch } from "react-router-dom";
 import "./api/axiosDefaults";
 import SignUpForm from "./pages/auth/SignUpForm";
 import SignInForm from "./pages/auth/SignInForm";
-import { createContext, useEffect, useState } from "react";
-import axios from "axios";
 
-export const CurrentUserContext = createContext();
-export const SetCurrentUserContext = createContext();
+
+
 
 function App() {
-  const [currentUser, setCurrentUser] = useState(null);
+ 
 
-  const handleMount = async () => {
-    try {
-      const { data } = await axios.get("dj-rest-auth/user/");
-      setCurrentUser(data);
-    } catch (err) {
-      console.log(err);
-    }
-  };
-
-  useEffect(() => {
-    handleMount();
-  }, []);
-
+ 
   return (
     <CurrentUserContext.Provider value={currentUser}>
     <SetCurrentUserContext.Provider value={setCurrentUser}>
