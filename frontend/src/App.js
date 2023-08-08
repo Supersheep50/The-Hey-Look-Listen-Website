b56126df1118697eb8c302beed7d7b6f316c13c6
Supersheep50 <jonathan.morrisseyto@gmail.com>
2023-08-08 10:42:45 +0000

    Add functionality to display likes and comments on posts

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,6 +6,7 @@ import "./api/axiosDefaults";
 import SignUpForm from "./pages/auth/SignUpForm";
 import SignInForm from "./pages/auth/SignInForm";
 import PostCreateForm from "./pages/posts/PostCreateForm";
+import PostPage from "./pages/posts/PostPage";
 
 
 
@@ -25,6 +26,7 @@ function App() {
           <Route exact path="/yarns" render={() => <h1>Gaming Yarns</h1>} />
           <Route exact path="/blog" render={() => <h1>Blog</h1>} />
           <Route exact path="/posts/create" render={() => <PostCreateForm />} />
+          <Route exact path="/posts/:id" render={() => <PostPage />} />
           <Route render={() => <p>Page not found!</p>} />
         </Switch>
       </Container>
