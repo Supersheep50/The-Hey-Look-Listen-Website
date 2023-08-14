e084a8feb6973d919ee272ef3238598bfbd66f67
Supersheep50 <jonathan.morrisseyto@gmail.com>
2023-08-14 09:13:52 +0000

    Add Readme tempalte from previous project

--- frontend/src/pages/profiles/PopularProfiles.js
+++ frontend/src/pages/profiles/PopularProfiles.js
@@ -41,7 +41,7 @@ const PopularProfiles = ({ mobile }) => {
     >
       {popularProfiles.results.length ? (
         <>
-          <p>Most followed profiles.</p>
+          <p>Biggest HLL fans!</p>
           {mobile ? (
             <div className="d-flex justify-content-around">
               {popularProfiles.results.slice(0, 4).map((profile) => (
